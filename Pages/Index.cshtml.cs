532f52c557ec9161cbc120bb7d380bcc70ca23a0
AntonioSkopin <askopin@outlook.com>
2020-08-12 11:06:49 +0200

    Secure password generator with ASP.NET Core

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PasswordGenerator2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnPost()
+        {
+            // String to be hashed
+            string password = "password";
+
+            // Generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            // Derive a 256-bit subkey (Use HMACSHA1 with 10,000 iterations)
+            @ViewData["generatedPassword"] = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8
+            ));
+            Console.WriteLine($"Generated password: {@ViewData["generatedPassword"]}");
+        }
+    }
+}
